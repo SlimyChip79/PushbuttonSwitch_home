15599571aea85e08f65b8533bf6a73ad58cd6415
SlimyChip79 <janis.logins11@gmail.com>
2025-12-21 13:58:48 +0200

    Update gaismas.py
    bru

--- gaismas.py
+++ gaismas.py
@@ -7,7 +7,7 @@ from adafruit_mcp230xx.digital_inout import DigitalInOut
 
 # -------------------- I2C SETUP --------------------
 i2c = busio.I2C(board.SCL, board.SDA)
-time.sleep(1)  # small delay for bus stability
+time.sleep(1)
 
 # MCP23017 input boards
 mcp1 = mcp23017.MCP23017(i2c, address=0x20)
@@ -18,55 +18,57 @@ pcf1 = PCF8575(i2c, address=0x26)
 pcf2 = PCF8575(i2c, address=0x27)
 
 # -------------------- INPUT SETUP --------------------
-# 32 pushbuttons mapped to 2 MCP23017 boards
-inputs_pins = list(range(16)) + list(range(16))  # 0–15 on each board
-inputs_board = [mcp1]*16 + [mcp2]*16
+inputs = []
+for pin in range(16):
+    p = DigitalInOut(mcp1.get_pin(pin))
+    p.switch_to_input()
+    inputs.append(p)
 
-inputs = [DigitalInOut(board, pin) for board, pin in zip(inputs_board, inputs_pins)]
-for p in inputs:
-    p.switch_to_input()  # default pull-down (reads LOW when pressed)
+for pin in range(16):
+    p = DigitalInOut(mcp2.get_pin(pin))
+    p.switch_to_input()
+    inputs.append(p)
 
 # -------------------- RELAY STATE --------------------
-relay_states = [False]*32  # all OFF initially
-# active-low -> HIGH = off
-pcf1.write_gpio(0xFFFF)
-pcf2.write_gpio(0xFFFF)
+relay_states = [False]*32  # all OFF
+out1 = 0xFFFF  # PCF8575 outputs start HIGH (OFF)
+out2 = 0xFFFF
+pcf1.write_gpio(out1)
+pcf2.write_gpio(out2)
 
 # -------------------- BUTTON STATE TRACKING --------------------
-last_input_values = [False]*32
+last_values = [0]*32
 
 print("32-input pushbutton controller started")
 
 # -------------------- MAIN LOOP --------------------
 while True:
-    for i, pin_obj in enumerate(inputs):
-        current_value = pin_obj.value
-        if current_value and not last_input_values[i]:  # rising edge
-            relay_states[i] = not relay_states[i]      # toggle relay
+    for i, inp in enumerate(inputs):
+        val = inp.value
+        if val and not last_values[i]:  # rising edge
+            relay_states[i] = not relay_states[i]
 
-        last_input_values[i] = current_value
+        last_values[i] = val
 
-        # write relays to PCF8575
-        relay_pin = i
-        if relay_pin < 16:
-            mask = 1 << relay_pin
+        # update outputs
+        if i < 16:
             if relay_states[i]:
-                pcf1.value &= ~mask  # ON
+                out1 &= ~(1 << i)  # LOW = ON
             else:
-                pcf1.value |= mask   # OFF
+                out1 |= (1 << i)   # HIGH = OFF
         else:
-            mask = 1 << (relay_pin - 16)
+            idx = i - 16
             if relay_states[i]:
-                pcf2.value &= ~mask  # ON
+                out2 &= ~(1 << idx)
             else:
-                pcf2.value |= mask   # OFF
+                out2 |= (1 << idx)
 
-    # apply relay states
-    pcf1.write_gpio(pcf1.value)
-    pcf2.write_gpio(pcf2.value)
+    # write outputs
+    pcf1.write_gpio(out1)
+    pcf2.write_gpio(out2)
 
     # optional debug
-    print([p.value for p in inputs])
-    print([int(r) for r in relay_states])
+    # print([inp.value for inp in inputs])
+    # print([int(r) for r in relay_states])
 
     time.sleep(0.05)
