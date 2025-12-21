b5d2a4874c6bca2e5cc00632b38f11c12c48b662
SlimyChip79 <janis.logins11@gmail.com>
2025-12-21 13:43:34 +0200

    Update gaismas.py
    change

--- gaismas.py
+++ gaismas.py
@@ -14,83 +14,43 @@ mcp_inputs = MCP23017(i2c, address=0x20)  # 16 inputs
 pcf_relays = PCF8575(i2c, address=0x26)   # 16 relays
 
 # -------------------- CONFIG --------------------
-DEBOUNCE_DELAY = 50         # ms
-LONG_PRESS_THRESHOLD = 2000 # ms
+DEBOUNCE_DELAY = 50  # ms
 
-# Split 16 inputs: 8 simple toggle, 8 click/hold
-simple_map = [(mcp_inputs, i) for i in range(8)]
-complex_map = [(mcp_inputs, i) for i in range(8, 16)]
-
-# Relays mapping: 8 for simple, 8 for complex
-simple_relays = list(range(8))
-complex_click_relays = list(range(8, 12))
-complex_hold_relays  = list(range(12, 16))
+# Map inputs to relays directly
+inputs_map = [(mcp_inputs, i) for i in range(16)]
+relays_map = list(range(16))
 
 # -------------------- INIT --------------------
-simple_inputs = [DigitalInOut(chip, pin) for chip, pin in simple_map]
-for p in simple_inputs: p.switch_to_input()
-
-complex_inputs = [DigitalInOut(chip, pin) for chip, pin in complex_map]
-for p in complex_inputs: p.switch_to_input()
+inputs = [DigitalInOut(chip, pin) for chip, pin in inputs_map]
+for p in inputs: p.switch_to_input()
 
 # Safety: all relays OFF (active-low)
 pcf_relays.write_gpio(0xFFFF)
 time.sleep(0.2)
 
 # States
-simple_states = [False]*len(simple_inputs)
-simple_last = [0]*len(simple_inputs)
+last_values = [0]*len(inputs)
+toggle_states = [False]*len(inputs)
 
-complex_last = [0]*len(complex_inputs)
-complex_last_debounce = [0]*len(complex_inputs)
-complex_press_start = [0]*len(complex_inputs)
-complex_long_triggered = [False]*len(complex_inputs)
-
-print("Simple + complex controller started")
+print("Simple toggle controller started")
 
 # -------------------- LOOP --------------------
 while True:
-    now = int(time.time()*1000)
     out = 0xFFFF  # all relays OFF
-
-    # --- SIMPLE TOGGLE ---
-    for i, pin_obj in enumerate(simple_inputs):
+    for i, pin_obj in enumerate(inputs):
         val = pin_obj.value
-        if val and not simple_last[i]:
-            simple_states[i] = not simple_states[i]
-        simple_last[i] = val
+        if val and not last_values[i]:
+            toggle_states[i] = not toggle_states[i]
+        last_values[i] = val
 
-        if simple_states[i]:
-            out &= ~(1 << simple_relays[i])
+        if toggle_states[i]:
+            out &= ~(1 << relays_map[i])
 
-    # --- COMPLEX CLICK/HOLD ---
-    for i, pin_obj in enumerate(complex_inputs):
-        val = pin_obj.value
-        if val != complex_last[i]:
-            complex_last_debounce[i] = now
-
-        if (now - complex_last_debounce[i]) > DEBOUNCE_DELAY:
-            if val:
-                if complex_press_start[i] == 0:
-                    complex_press_start[i] = now
-                    complex_long_triggered[i] = False
-                elif not complex_long_triggered[i] and (now - complex_press_start[i]) >= LONG_PRESS_THRESHOLD:
-                    out &= ~(1 << complex_hold_relays[i])
-                    complex_long_triggered[i] = True
-            else:
-                if complex_press_start[i] != 0 and not complex_long_triggered[i]:
-                    out &= ~(1 << complex_click_relays[i])
-                complex_press_start[i] = 0
-                complex_long_triggered[i] = False
-
-        complex_last[i] = val
-
-    # --- WRITE RELAYS ---
+    # Write relays
     pcf_relays.write_gpio(out)
 
-    # --- DEBUG ---
-    print("Simple:", [p.value for p in simple_inputs])
-    print("Complex:", [p.value for p in complex_inputs])
+    # Debug
+    print("Inputs:", [p.value for p in inputs])
     print("Relays: {:016b}".format(out))
 
     time.sleep(0.05)
