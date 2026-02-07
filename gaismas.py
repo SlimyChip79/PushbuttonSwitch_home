8ca26075bab34a1e689b12ec88b59931111891bb
SlimyChip79 <janis.logins11@gmail.com>
2026-02-07 18:09:08 +0200

    Update gaismas.py
    fix

--- gaismas.py
+++ gaismas.py
@@ -1,29 +1,15 @@
-#!/usr/bin/env python3
 from pcf8575 import PCF8575
 import time
 
 PCF_ADDR = 0x27
-pcf = PCF8575(1, PCF_ADDR)  # bus 1
+pcf = PCF8575(PCF_ADDR)  # just address
 
-# setup all 16 pins as outputs
-for i in range(16):
-    pcf.setup(i, pcf.OUT)
-    pcf.write(i, 1)  # 1 = off (active-low)
+while True:
+    for pin in range(16):
+        pcf.digital_write(pin, 0)  # turn relay ON
+        print(f"Relay {pin+1} ON")
+        time.sleep(0.5)
 
-try:
-    while True:
-        for i in range(16):
-            # turn ON relay
-            pcf.write(i, 0)
-            print(f"Relay {i+1} ON")
-            time.sleep(0.5)
-
-            # turn OFF relay
-            pcf.write(i, 1)
-            print(f"Relay {i+1} OFF")
-            time.sleep(0.2)
-
-except KeyboardInterrupt:
-    print("Stopping, turning all relays off...")
-    for i in range(16):
-        pcf.write(i, 1)
+        pcf.digital_write(pin, 1)  # turn relay OFF
+        print(f"Relay {pin+1} OFF")
+        time.sleep(0.2)
