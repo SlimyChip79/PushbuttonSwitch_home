fca5e39f285884f981769c8e572d380c50b4ad1c
SlimyChip79 <janis.logins11@gmail.com>
2026-02-10 21:26:13 +0200

    Update gaismas.py
    something bit bigger

--- gaismas.py
+++ gaismas.py
@@ -1,23 +1,62 @@
+#!/usr/bin/env python3
 import time
-import board
-import busio
-from adafruit_pcf8575 import PCF8575
+from pcf8575 import PCF8575
+from smbus2 import SMBus
 
-# -------------------- I2C SETUP --------------------
-i2c = busio.I2C(board.SCL, board.SDA)
-time.sleep(1)  # small delay for bus stability
+# -------------------- CONFIG --------------------
+PCF_ADDR = 0x27
+PCA_ADDR = 0x20
+I2C_BUS = 1
 
-# -------------------- PCF8575 RELAYS --------------------
-pcf = PCF8575(i2c, address=0x27)
+# PCA registers
+REG_OUTPUT_1 = 0x03
+REG_CONFIG_0 = 0x06
+REG_CONFIG_1 = 0x07
 
-# set all relays OFF initially (active-low)
-pcf.write_gpio(0xFFFF)
+# -------------------- INIT --------------------
+# PCF8575
+pcf = PCF8575(I2C_BUS, PCF_ADDR)
+for i in range(16):
+    pcf.pin(i, 1)  # all OFF
 
-# -------------------- MAIN LOOP --------------------
-while True:
-    for i in range(16):
-        mask = 1 << i
-        # turn only this relay ON, rest OFF
-        pcf.write_gpio(~mask & 0xFFFF)
-        print(f"Relay {i} ON")
+# PCA9555
+bus = SMBus(I2C_BUS)
+bus.write_byte_data(PCA_ADDR, REG_CONFIG_0, 0xFF)  # P0 inputs (unused)
+bus.write_byte_data(PCA_ADDR, REG_CONFIG_1, 0x00)  # P1 outputs
+bus.write_byte_data(PCA_ADDR, REG_OUTPUT_1, 0xFF)  # all OFF (active low)
+
+# -------------------- RELAY CONTROL FUNCTION --------------------
+def set_relay(index, state):
+    """
+    index: 0..31
+    state: True=ON, False=OFF (active-low assumed)
+    """
+    if index < 16:
+        # PCF8575
+        pcf.pin(index, 0 if state else 1)
+    else:
+        # PCA9555
+        relay_idx = index - 16
+        val = bus.read_byte_data(PCA_ADDR, REG_OUTPUT_1)
+        if state:
+            val &= ~(1 << relay_idx)  # ON = 0
+        else:
+            val |= (1 << relay_idx)   # OFF = 1
+        bus.write_byte_data(PCA_ADDR, REG_OUTPUT_1, val)
+
+# -------------------- EXAMPLE USAGE --------------------
+try:
+    # Example: turn some relays ON
+    set_relay(0, True)    # PCF first relay ON
+    set_relay(15, True)   # PCF last relay ON
+    set_relay(16, True)   # PCA first relay ON
+    set_relay(31, True)   # PCA last relay ON
+
+    # Keep running, you can later poll inputs or set relays dynamically
+    while True:
         time.sleep(0.5)
+
+except KeyboardInterrupt:
+    # turn everything off before exit
+    for i in range(32):
+        set_relay(i, False)
