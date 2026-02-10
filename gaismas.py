a785e43bf5c68de9a65396ddb3f84c0a3d03d2f5
SlimyChip79 <janis.logins11@gmail.com>
2026-02-10 21:16:31 +0200

    Update gaismas.py
    (￣y▽,￣)╭

--- gaismas.py
+++ gaismas.py
@@ -3,22 +3,21 @@ import board
 import busio
 from adafruit_pcf8575 import PCF8575
 
+# -------------------- I2C SETUP --------------------
 i2c = busio.I2C(board.SCL, board.SDA)
-time.sleep(1)
+time.sleep(1)  # small delay for bus stability
 
+# -------------------- PCF8575 RELAYS --------------------
 pcf = PCF8575(i2c, address=0x27)
 
-# All relays OFF initially (active-low)
-pcf.value = 0xFFFF
+# set all relays OFF initially (active-low)
+pcf.write_gpio(0xFFFF)
 
+# -------------------- MAIN LOOP --------------------
 while True:
-    for i in range(8):
+    for i in range(16):
         mask = 1 << i
-
-        # Turn ON
-        pcf.value &= ~mask
-        time.sleep(0.5)
-
-        # Turn OFF
-        pcf.value |= mask
+        # turn only this relay ON, rest OFF
+        pcf.write_gpio(~mask & 0xFFFF)
+        print(f"Relay {i} ON")
         time.sleep(0.5)
